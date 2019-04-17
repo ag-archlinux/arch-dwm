c7feb4bb18fcb71c9cf44b245ab192f89359aad4
ag-archlinux <ag.archlinux@protonmail.com>
2019-04-18 01:16:46 +0200

    ARCH LNUX DWM

---
+++ conf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#####     Created by: ag
+#####     --------------------------------------------------
+#####     1. Configuration
+	##### a) xorg
+		sudo pacman --noconfirm --needed -Syu
+		sudo pacman --noconfirm --needed -S xorg-server xorg-xinit xorg-xsetroot bash-completion gcc make pkg-config libx11 libxft libxinerama ttf-ubuntu-font-family
+		sudo pacman --noconfirm --needed -S filezilla gimp inkscape firefox neovim rxvt-unicode zip unrar unzip ranger htop
+		sudo pacman --noconfirm --needed -S scrot w3m lynx atool highlight xclip mupdf mplayer transmission-cli openssh
+		sudo pacman --noconfirm --needed -S ncmpcpp pulseaudio-alsa pulsemixer	wget zathura conky 
+		sudo pacman --noconfirm --needed -S nitrogen compton youtube-dl sxiv entr
+		sudo pacman --noconfirm --needed -S gimp kodi qrencode netcat feh mediainfo
+		sudo pacman --noconfirm --needed -S termbin noto-fonts neomutt urlview
+		#lspci | grep -e VGA -e 3D
+    ##### b) git
+    	cd
+		sudo pacman --noconfirm --needed -S git
+		git clone https://git.suckless.org/dwm
+		git clone https://git.suckless.org/dmenu
+		git clone https://git.suckless.org/st
+		git clone https://git.suckless.org/surf
+		
+		cd ~/dwm/   && make clean install
+		cd ~/dmenu/ && make clean install
+		cd ~/st/   && make clean install
+		cd ~/surf/ && make clean install
+	##### c) copy my config files
+		cp ~/arch-dwm/home/.xinitrc ~/.xinitrc
+	##### d) startx
+		startx
