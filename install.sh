6f97c001af6404a9029bda1ee2dce95e3c97e5ca
ag-archlinux <ag.archlinux@protonmail.com>
2019-04-23 03:19:35 +0200

    ARCH LNUX DWM

--- install.sh
+++ install.sh
@@ -159,45 +159,43 @@ EOF
     	curl -LO https://raw.githubusercontent.com/ag-archlinux/arch-dwm/master/install.sh 
     	cp install.sh /etc/skel/script.sh
     	echo "bash /etc/skel/script.sh" >> /etc/skel/.bash_profile
-    	read -p "END"
-    	read -p "END"
 	##### c) Chroot
-        arch-chroot /mnt <<EOF
-#####     --------------------------------------------------
-    ##### b) Chroot
-
-    	##### 1) Time zone
-    		ln -sf /usr/share/zoneinfo/Europe/Bratislava /etc/localtime
-    		hwclock-systohc
-    	##### 2) Locale
-    		echo "en_US.UTF-8 UTF-8" >> /etc/local.gen
-			locale-gen
-			echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-    	##### 3) Hostname
-    		read -p "Enter your computer's name: " COMPUTER_NAME
-    		echo $COMPUTER_NAME >> /etc/hostname
-			echo "127.0.0.1  localhost" >> /etc/hosts
-			echo "::1        localhost" >> /etc/hosts
-			echo "127.0.0.1  " + $COMPUTER_NAME+ ".localdomain "+ $COMPUTER_NAME >> /etc/hosts
-    	##### 4) Network configuration
-			# Configure network manager
-    		pacman --noconfirm --needed -S networkmanager
-			systemctl enable NetworkManager
-			systemctl start NetworkManager
-			pacman --noconfirm --needed -S iw wpa_supplicant dialog wpa-actiond
-			systemctl enable dhcpcd
-    	##### 5) Initramfs
-    		mkinitcpio -p linux
-    	##### 6) Root password
-    		echo "Enter root password: "
-			passwd
-    	##### 7) Boot loader
-    		pacman --noconfirm --needed -S grub os-prober
-			grub-install --recheck --target=i386-pc /dev/sda
-			grub-mkconfig -o /boot/grub/grub.cfg
-	##### Exit chroot
-	exit
-#####     --------------------------------------------------
+        arch-chroot /mnt /bin/bash <<EOF
+	    	##### 1) Time zone
+    			ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    			hwclock-systohc
+    		##### 2) Locale
+    			sed -i "s|#\($LOCALE.*\)\$|\1|" /etc/locale.gen
+                locale-gen
+                echo "LANG=$LOCALE" >> /etc/locale.conf
+	    	##### 3) Hostname
+    			echo $HOSTNAME >> /etc/hostname
+				echo "127.0.0.1  localhost" >> /etc/hosts
+				echo "::1        localhost" >> /etc/hosts
+				echo "127.0.0.1  " + $HOSTNAME+ ".localdomain "+ $HOSTNAME >> /etc/hosts
+    		##### 4) Network configuration
+				##### a) create personal account & password of personal account
+    				useradd -k /etc/skel -m -g users -G audio,video,network,wheel,storage -s /bin/bash $USERNAME
+    				echo "$USERNAME:$USER_PASSWORD" | /usr/sbin/chpasswd
+    			##### b) account sudo permitions
+					sed -i "/#MY_PERMISSION/d" /etc/sudoers
+					echo -e "%wheel ALL=(ALL) NOPASSWD: ALL #MY_PERMISSION" >> /etc/sudoers    		
+				##### c) configure network manager
+    				pacman --noconfirm --needed -S networkmanager
+					systemctl enable NetworkManager
+					systemctl start NetworkManager
+				  	#pacman --noconfirm --needed -S iw wpa_supplicant dialog wpa-actiond
+				  	#systemctl enable dhcpcd
+    		##### 5) Initramfs
+    			mkinitcpio -p linux
+    		##### 6) Root password
+    			echo "root:$ROOT_PASSWORD" | /usr/sbin/chpasswd
+    		##### 7) Boot loader
+    			pacman --noconfirm --needed -S grub os-prober
+				grub-install --recheck --target=i386-pc $DRIVE
+				grub-mkconfig -o /boot/grub/grub.cfg
+			##### Exit chroot
+				exit
 EOF
     ##### d) Unmount all the partitions
     	umount -R /mnt
